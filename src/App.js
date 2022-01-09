0cbe53f2e88a920a9d79b63cf1b1a1a66e8ebbf1
GGolfz <wisarutgolf11@gmail.com>
2022-01-09 16:32:07 +0700

    add: all :sparkles:

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { initializeApp } from "firebase/app";
+import { getStorage, ref, uploadBytes } from "firebase/storage";
+import { useState } from "react";
+const firebaseConfig = {
+  apiKey: '<your-api-key>',
+  authDomain: '<your-auth-domain>',
+  storageBucket: '<your-storage-bucket-url>'
+};
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
 function App() {
+  const [file, setFile] = useState(null);
+  const handleUpload = () => {
+    const imageRef = ref(storage, "images/" + file.name);
+    uploadBytes(imageRef, file).then((snapshot) => {
+      console.log(snapshot);
+      alert("Upload Success !");
+    });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="file"
+        id="image"
+        onChange={(e) => setFile(e.target.files[0])}
+        multiple={false}
+        accept="image/*"
+      />
+      <button onClick={handleUpload}>Upload</button>
     </div>
   );
 }
